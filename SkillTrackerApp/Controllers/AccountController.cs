9b97bf9360b71b5099a667024ad747305c9b47ac
Simthanda Khanyile <KhanyileS@dutappfactory.tech>
2025-07-26 04:59:58 +0200

    Account controller fixed up and working now register page

--- SkillTrackerApp/Controllers/AccountController.cs
+++ SkillTrackerApp/Controllers/AccountController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SkillTrackerApp.Models;
 using SkillTrackerApp.ViewModels;
 
 namespace SkillTrackerApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<Users> signInManager;
+        private readonly UserManager<Users> userManager;
+
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -15,6 +26,36 @@ namespace SkillTrackerApp.Controllers
             return View(); 
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users users = new Users
+                {
+                    FullName = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email
+                };
+
+                var result = await userManager.CreateAsync(users, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                        return View(model);
+                    }
+                }
+                return View(model);
+            }
+            return View(model);
+        } 
         public IActionResult VerifyEmail()
         {
             return View();
